324bee397feab21d8a850225a35b63f58e969e23
Samir Remtulla <samir.remtulla@gmail.com>
2013-04-21 21:51:47 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Leadgen::Application.config.secret_token = 'c6c86be57897fb880a988d17ec3f4d6a6a2ccfe34c0a51881b373aa4cbe85685be5132e68a101d90763b1cb7dc9282d4f24161d29739c95b57987fab5553546b'
