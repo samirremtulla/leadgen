c2a8eeb025be84d005e508c262a61d240320fcbc
Samir Remtulla <samir.remtulla@gmail.com>
2013-04-25 16:26:40 -0400

    pusher ID reset

--- config/environment.rb
+++ config/environment.rb
@@ -6,6 +6,6 @@ Leadgen::Application.initialize!
 
 require 'pusher'
 
-Pusher.app_id = '42395'
-Pusher.key = 'ea68d0e9ff775535a631'
-Pusher.secret = '2b13d8c8b1bf988147a2'
+Pusher.app_id = ENV["PUSHER_APP_ID"]
+Pusher.key = ENV["PUSHER_KEY"]
+Pusher.secret = ENV["PUSHER_SECRET"]
