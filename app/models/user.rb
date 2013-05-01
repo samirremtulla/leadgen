1da4ba645b0f6ce208320688f7a01d1f0e5402a6
Samir Remtulla <samir.remtulla@gmail.com>
2013-05-01 12:48:03 -0400

    minor user ui changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
   # attr_accessible :title, :body
-    authenticates_with_sorcery!
+  authenticates_with_sorcery!
   has_many :payments
   has_many :questions, :through => :payments
 
